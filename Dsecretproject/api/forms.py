897d918a0350c6ba37ff6ea4008e4102a7d6746b
0euun <jeyejong70@naver.com>
2025-06-27 20:20:55 +0900

    설영은: 회원가입 css 추가 및 forms.py 수정

--- Dsecretproject/api/forms.py
+++ Dsecretproject/api/forms.py
@@ -1,7 +1,21 @@
+from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from .models import *
 
 class RegisterForm(UserCreationForm):
+    password1 = forms.CharField(
+        label="Password",
+        widget=forms.PasswordInput,
+        help_text=''
+    )
+    password2 = forms.CharField(
+        label="Password confirmation",
+        widget=forms.PasswordInput,
+        help_text=''
+    )
     class Meta:
         model = User
         fields = ['username', 'password1', 'password2']
+        help_texts = {
+            'username': '',
+        }
