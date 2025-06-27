63616e498e0cca767e311c5da4657cdef0ad8e46
0euun <jeyejong70@naver.com>
2025-06-27 20:37:59 +0900

    설영은: 회원가입 최종 수정

--- Dsecretproject/api/forms.py
+++ Dsecretproject/api/forms.py
@@ -1,21 +1,29 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from .models import *
+from .models import User
 
 class RegisterForm(UserCreationForm):
     password1 = forms.CharField(
         label="Password",
-        widget=forms.PasswordInput,
-        help_text=''
+        widget=forms.PasswordInput(attrs={'placeholder': 'Enter your password'})
     )
     password2 = forms.CharField(
-        label="Password confirmation",
-        widget=forms.PasswordInput,
-        help_text=''
+        label="Confirm Password",
+        widget=forms.PasswordInput(attrs={'placeholder': 'Confirm your password'})
     )
     class Meta:
         model = User
-        fields = ['username', 'password1', 'password2']
+        fields = ['username', 'email', 'password1', 'password2']
+        widgets = {
+            'username': forms.TextInput(attrs={'placeholder': 'Enter your username'}),
+            'email': forms.EmailInput(attrs={'placeholder': 'Enter your email'}),
+        }
+        labels = {
+            'password2': 'Confirm Password',
+        }
         help_texts = {
             'username': '',
+            'email': '',
+            'password1': '',
+            'password2': '',
         }
