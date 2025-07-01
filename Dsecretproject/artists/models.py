30262e99f4a36778a3711ea0c4f8fd768b425bee
0euun <jeyejong70@naver.com>
2025-07-01 09:55:01 +0900

    aritsts 앱 추가, 모델 설정

---
+++ Dsecretproject/artists/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class Artist(models.Model):
+    name = models.CharField(max_length=10)
+    team = models.CharField(max_length=200)
+    birthday = models.DateField()
+    position = models.CharField(max_length=100)
+    # profile = models.ImageField() 추후 수정
+
+    def __str__(self):
+        return self.name
