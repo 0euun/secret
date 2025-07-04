48b12f56e9086ef7ee9c8fa4d23d906012959e72
0euun <jeyejong70@naver.com>
2025-07-04 21:47:34 +0900

    blog 앱 urls.py 생성

--- Dsecretproject/secretproject/urls.py
+++ Dsecretproject/secretproject/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('', login_view, name='root_login'),
     path('api/', include('api.urls')),
     path('artists/', include('artists.urls')),
+    path('blog/', include('blog.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
